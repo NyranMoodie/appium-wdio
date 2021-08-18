18f6b82787eabd872ee071c2a38cc8cc7f199adb
NyranMoodie <nyranmoodie@gmail.com>
2021-08-18 06:54:42 -0500

    add login steps

--- test/specs/login.js
+++ test/specs/login.js
@@ -1,30 +1,23 @@
-const LoginScreen = require('../screenObjects/LoginScreen.page');
-const TabBar = require('../screenObjects/components/TabBar.page');
+const TabBar = require('../screenObjects/components/TabBar');
+const LoginScreen = require('../screenObjects/LoginScreen');
 
-describe('WebdriverIO and Appium, when interacting with a login form,', () => {
+describe('WebdriverIO and Appium, when using navigation through the tab bar', () => {
 
     beforeEach(() => {
         TabBar.waitForTabBarShown();
         TabBar.openLogin();
         LoginScreen.waitForIsShown()
-    })
-
-    it('should be able login successfully', async () => {
-        // Always make sure you are on the right tab
-        LoginScreen.clickLoginContainerButton()
-        // Submit the data
-        LoginScreen.submitLoginForm('test@webdriver.io', 'Test1234!');
-
-        await browser.pause(5000)
-        // Wait for the alert and validate it
-        // NativeAlert.waitForIsShown();
-        // expect(NativeAlert.text()).toEqual('Success\nYou are logged in!');
-
-        // // Close the alert
-        // NativeAlert.pressButton('OK');
-        // NativeAlert.waitForIsShown(false);
     });
 
+    it('should be able to open the webview', async () => {
+        // Always make sure you are on the right tab
+        await LoginScreen.loginContainerButton.click();
+        // Submit the data
+        await LoginScreen.submitLoginForm('ss@dd.com', 'Password1!')
+
+        await browser.pause(1000)
+        // WebViewScreen.waitForWebsiteLoaded();
+    });
 });
 
 
